55ca74c76125538a31de98e655288408a270e0ef
Amanda Prampero <amandapramperomendes@gmail.com>
2024-01-30 11:29:51 -0300

    desafio 74

---
+++ Mundo3/menorMaior_tupla.py
@@ -0,0 +1,13 @@
+'''Crie um programa que vai gerar cinco números aleatórios
+    e colocar em uma tupla. Depois disso, mostre a listagem
+    de números gerados e também indique o menor e o maior 
+    valores que estão na tupla.'''
+
+from random import randint
+
+numeros = (randint(0,11), randint(0,11), randint(0,11), randint(0,11), randint(0,11))
+print(f'Os valores sorteados foram: ', end = '')
+for n in numeros:
+    print(f'{n} ', end = '')
+print(f'\nO maior é: {max(numeros)}')
+print(f'O menor é: {min(numeros)}')
