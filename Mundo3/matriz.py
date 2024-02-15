2688ba89afd21add53dac78ed1befc4bf6de5c57
Amanda Prampero <amandapramperomendes@gmail.com>
2024-02-15 15:54:54 -0300

    desafio 87

--- Mundo3/matriz.py
+++ Mundo3/matriz.py
@@ -4,6 +4,7 @@
     mostre a matriz na tela, com a formatação correta.
 '''
 matrix = [[0,0,0], [0,0,0], [0,0,0]]
+soma_pares = soma_coluna = 0
 
 print('\033[35m**\033[m' * 20)
 print('\t\tMATRIZ')
@@ -16,5 +17,21 @@ for linha in range(0,3):
 print('\033[35m**\033[m' * 20)
 for linha in range(0, 3):
   for coluna in range(0, 3):
+    if matrix[linha][coluna] % 2 == 0:
+      soma_pares += matrix[linha][coluna]
+    if coluna == 2:
+      soma_coluna += matrix[linha][coluna]
     print(f'[{matrix[linha][coluna]:^5}]', end='')
   print()
+
+'''
+    Aprimore o desafio anterior, mostrando no final:
+    A) A soma de todos os valores pares digitados.
+    B) A soma dos valores da terceira coluna.
+    C) O maior valor da segunda linha.
+'''
+print('\033[35m**\033[m' * 20)
+maior = max(matrix[1])
+print(f'Soma doa valores pares digitados: {soma_pares}')
+print(f'A soma dos valores da terceira coluna: {soma_coluna}')
+print(f'O maior valor da segunda linha: {maior}')
